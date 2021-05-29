d8742337f298a1d8e93ee6a3c4620b82a0428018
DianRP43 <dianrosapratama2000@gmail.com>
2021-05-28 21:51:25 -0800

    selesai

---
+++ app/src/main/java/com/example/quiz10118043/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quiz10118043;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//(29 mei,10118043,DianRosaPratama,IF01)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
